cff00514831d1d4469f421aa3b6806d26ba09774
M Rumman Hasan <mrayyanhkhan@outlook.com>
2018-09-28 23:22:18 +0500

    bot telling doctor time

--- pymysql/demo.py
+++ pymysql/demo.py
@@ -7,51 +7,64 @@ a = conn.cursor()
 # end connection work
 
 print("Hi")
-greetings = ['hola', 'hello', 'hi', 'Hi', 'hey!', 'hey']
+greetings = ['hola', 'hello', 'hi', 'hi!', 'hey!', 'hey', 'salam']
 random_greeting = random.choice(greetings)
 question = ['How are you?', 'How are you doing?']
 
-goodResponses = ['okay', "I'm fine", 'good']
+goodResponses = ['okay', "i'm fine", 'good', 'fine']
 random_response = random.choice(goodResponses)
 
 docTypeQue = ['For what problem you want to see doctor?']
-docTypeResp = ['Eye', 'Nose', 'Throut']
-docTypeAmbigiousResp = ["Don't know", 'No idea', 'Just feeling ill']
+docTypeResp = ['eye', 'nose', 'throat']
+docTypeAmbigiousResp = ["don't know", 'no idea', 'just feeling ill']
 
 # working logic
 while True:
     userInput = input(">>> ")
-    if userInput in greetings:
-        print(random_greeting + "\n " + random.choice(question))
-    elif userInput in goodResponses:
-        print("For what problem you want to see doctor? ")
-    elif userInput in docTypeResp:
-        print("No problem we shall connect you to " + userInput + " Specialist")
-        if userInput in docTypeResp:
-            # execute query
-            sql = 'SELECT * FROM `timings` WHERE docType = "ENT";'
-            a.execute(sql)
-            data = a.fetchone()
-            print(data)
-        else:
-            # General physician query
-            sql = 'SELECT * FROM `timings` WHERE docType = "General";'
-            a.execute(sql)
-            data = a.fetchmany()
-            print(data)
+    if userInput.lower().strip() in greetings or userInput.lower().strip() in goodResponses:
+        if userInput.lower().strip() in greetings:
+            print("\n" + random.choice(question))
 
-    elif userInput in docTypeAmbigiousResp:
-        print("Sorry about that... \n No problem we shall connect you to the Doctor")
-        # query for physician
+        elif userInput.lower().strip() in goodResponses:
+            
+            userInput = input(">>> For what problem you want to see doctor? ")
+
+            if userInput.lower().strip() in docTypeResp:
+                print("No problem we shall connect you to " + userInput + " Specialist")
+                # execute query
+                sql = 'SELECT docName,docType,docTimeFrom,docTimeTo,docDay FROM `timings` WHERE docType = "ENT";'
+                a.execute(sql)
+                data = a.fetchall()
+                # print(data)
+                for entry in data:
+                    print("Dr. " + str(entry[0]) + "\n " + str(entry[1]) + "\n " + str(
+                        entry[2])[-5:] + " to " + str(entry[3]) + "\n " + str(entry[4])+"\n")
+                print("Book Your appointment \n BYE Take Care")
+                break
+
+            else:
+                # General physician query
+                sql = 'SELECT docName,docType,docTimeFrom,docTimeTo,docDay FROM `timings` WHERE docType = "General";'
+                a.execute(sql)
+                data = a.fetchall()
+                # print(data)
+                for entry in data:
+                    print("Dr. " + str(entry[0]) + "\n " + str(entry[1]) + "\n " + str(entry[2])[-5:] + " to " + str(entry[3]) + "\n " + str(entry[4])+"\n")
+                break
+
+    
     else:
         print("I did not understand what you said")
 
 
 # sql = 'SELECT * FROM `timings`;'
 # a.execute(sql)
-# countrow = a.execute(sql)
-
-# print("Number of rows: ", countrow)
-# data = a.fetchone()
-
+# data = a.fetchall()
 # print(data)
+
+# i = 0
+# for entry in data:
+#     print("Dr. "+ str(entry[0]) +"\n "+ str(entry[1]) +"\n "+ str(entry[2])[-5:] +" to "+ str(entry[3]) +"\n "+ str(entry[4])+"\n")
+#     # for e in entry:
+#         # print(e)
+#         # i = i + 1
